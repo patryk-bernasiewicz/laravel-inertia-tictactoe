1c9044c15db1e9a2cdc501485283d5dd55de8c1a
Patryk Bernasiewicz <patryk.bernasiewicz@gmail.com>
2024-03-10 14:16:02 +0100

    base init

---
+++ resources/js/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    let foo: string = 'React';
+    const bar: string = 'TypeScript';
+
+    return (
+        <h1>Hello {foo} + {bar}!!!</h1>
+    );
+};
+
+export default Home;
---
+++ resources/js/app.jsx
@@ -0,0 +1,17 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/inertia-react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+createInertiaApp({
+    resolve: (name) => {
+        return resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx'));
+    },
+    setup({ el, App, props }) {
+        createRoot(el).render(
+            <StrictMode>
+                <App {...props} />
+            </StrictMode>
+        );
+    },
+});
